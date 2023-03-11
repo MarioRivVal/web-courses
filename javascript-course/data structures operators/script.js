691c731bcbeb86dd8399abdf5395b6b49905df0e
Mario Valverde Rivera <rival.moro@hotmail.it>
2023-03-11 13:04:56 +0000

    the spread operator

--- javascript-course/data structures operators/script.js
+++ javascript-course/data structures operators/script.js
@@ -31,19 +31,111 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  // ejemplo 2
+  // cuando por ejemplo tenemos una funcion que recibira un objeto pero no sabemos el orden de los parametros simplemente le pasamos un objeto como parametro y la funcion lo destructurara automaticamente: function(obj)
 
-  orderDelivery: function({obj}){
-    console.log(obj);
-    // console.log(`Order recived! ${this.mainMenu[mainIndex]} and ${this.starterMenu[starterIndex]} will be delivered at ${time} to ${address}`);
-  }
+  //   orderDelivery: function(obj){
+  //     console.log(obj);
+  // }
+
+  //una vez que tenemos el argumento objeto entonces podemos destrurturarla con el nombre de cada elemento:
+
+  // tambien podemos especificar valores predefinidos en algun parametro:
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order recived! ${this.mainMenu[mainIndex]} and ${this.starterMenu[starterIndex]} will be delivered at ${time} to ${address}`
+    );
+  },
+
+  // ejemplo 3
+  // creamos un method que reciva multiples arguments y usamos el spread operator para pasar esos argumentos
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`here your pasta with ${ing1},${ing2},${ing3}`);
+  },
 };
 
+// llamando el ejemplo 2
+// aqui llamamos la funcion con sus valores
 restaurant.orderDelivery({
-  time:'22:30',
-  address:'via del sole, 21',
-  mainIndex:2,
-  starterIndex:2
-})
+  time: '22:30',
+  address: 'via del sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+//aqui llamamos la funcion con los valores predeterminados
+
+restaurant.orderDelivery({
+  address: 'via del sole, 21',
+});
+
+// -----  THE SPREAD OPERATOR (...) (operador de propagacion)---- //
+
+// el operador de propagacion nos ayuda a descomponer un array y unir sus elementos uno por uno separados por un coma
+
+const array = [7, 8, 9];
+const badNewArray = [1, 2, array[0], array[1], array[2]];
+console.log(badNewArray); // funciona pero no es la manera correcta con las nuevas tecnicas
+
+// si no la siguiente
+const goodNewArry = [1, 2, ...array];
+console.log(goodNewArry);
+
+// otro ejemplo para expandir un array
+console.log(...goodNewArry);
+// esto seria como escribir esto
+console.log(1, 2, 7, 8, 9);
+
+// ejemplo si quisieramos crear un nuevo menu para el restaurante
+const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+console.log(newMenu);
+// tomar nota que aqui se crea un nuevo array y no see es manipulando el original restaurant.mainMenu
+
+//NOTA: el spread operator tambien funciona como destruturador pero no crea nuevas variables y abarca todos los elementos del array.
+
+//---- copying an array
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy, restaurant.mainMenu);
+
+// ---uniendo dos array
+
+const fullMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(...fullMenu);
+
+// Iterables: arrays strings, maps, sets. pero NO abjects
+// entonces podemos descomponer una string en elementos individuales
+
+const string = 'Mario';
+const lettersString = [...string, '', 'R'];
+console.log(lettersString);
+console.log(...lettersString);
+console.log(...string);
+
+//NOTA: recordar que valores multiplos separados por coma solo se esperan cuando pasamos argumentos en una funcion o cuando creamos arrays
+
+// llamamos el ejemplo 3
+const ingredients = ['oil', 'zucchini', 'potates'];
+// const ingredients = [prompt('ing1?'),prompt('ing2?'),prompt('ing3?')]
+// lo ocultamos para no confundir la pagina
+restaurant.orderPasta(...ingredients);
+
+// con las nuevas funciones de js podemos tambien usar el spread operator en objetos auqnue si no son iterables
+const newRestaurant = {
+  foundedYear: '1998',
+  ...restaurant,
+  founder: 'Mario Rivera',
+}; // unimos restaurant y agregamos datos a un nuevo objeto newRestaurant
+console.log(newRestaurant);
+
+// tambien podemos copiar todo un objeto y al mismo tiempo poder cambiar algun dato sin alterar el original
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Restaurante Roma'; // cambiamos el nombre del restaurante en restaurantCopy
+console.log(restaurant.name, restaurantCopy.name);
 
 // -----  DESTRUCTURING OBJECTS---- //
 
